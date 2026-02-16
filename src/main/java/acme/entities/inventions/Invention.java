ab7ad2c338607937a6712eb33d1625e4f8a4dc6b
José Javier Morán Corbacho <josmorcor3@alum.us.es>
2026-02-16 11:10:44 +0100

    Cambiar tipo Date por Moment

--- src/main/java/acme/entities/inventions/Invention.java
+++ src/main/java/acme/entities/inventions/Invention.java
@@ -1,8 +1,6 @@
 
 package acme.entities.inventions;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -11,12 +9,13 @@ import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoment.Constraint;
-import acme.realms.Inventor;
 import acme.client.components.validation.ValidUrl;
+import acme.realms.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -49,12 +48,12 @@ public class Invention extends AbstractEntity {
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				startMoment;
+	private Moment				startMoment;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				endMoment;
+	private Moment				endMoment;
 
 	@Optional
 	@ValidUrl
