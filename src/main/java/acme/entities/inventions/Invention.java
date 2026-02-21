4efffe3e1d65fcfd8c7cc654007364c1929c2890
José Javier Morán Corbacho <josmorcor3@alum.us.es>
2026-02-21 14:52:09 +0100

    Implementar requisito S1/1 con validaciones personalizadas y adicionales

--- src/main/java/acme/entities/inventions/Invention.java
+++ src/main/java/acme/entities/inventions/Invention.java
@@ -1,20 +1,25 @@
 
 package acme.entities.inventions;
 
+import java.time.Duration;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Moment;
+import acme.client.components.datatypes.Money;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoment.Constraint;
 import acme.client.components.validation.ValidUrl;
+import acme.client.helpers.MomentHelper;
 import acme.realms.Inventor;
 import lombok.Getter;
 import lombok.Setter;
@@ -47,12 +52,12 @@ public class Invention extends AbstractEntity {
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
-	@Temporal(TemporalType.TIMESTAMP)
+	@Column
 	private Moment				startMoment;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
-	@Temporal(TemporalType.TIMESTAMP)
+	@Column
 	private Moment				endMoment;
 
 	@Optional
@@ -67,11 +72,39 @@ public class Invention extends AbstractEntity {
 
 	// Derived attributes -----------------------------------------------------
 
+	@Transient
+	@Autowired
+	private InventionRepository	repository;
+
+
+	@Transient
+	public Double getMonthsActive() {
+		Duration duration = MomentHelper.computeDuration(this.startMoment, this.endMoment);
+		double months = duration.toDays() / 30.;
+		return Math.round(months * 10) / 10.;
+	}
+
+	@Transient
+	public Money getCost() {
+		Money cost = new Money();
+		Double result;
+
+		cost.setCurrency("EUR");
+		result = this.repository.computeInventionCost(this.getId());
+		if (result == null)
+			cost.setAmount(0.0);
+		else
+			cost.setAmount(result);
+
+		return cost;
+	}
+
 	// Relationships ----------------------------------------------------------
+
 
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
-	private Inventor			inventor;
+	private Inventor inventor;
 
 }
