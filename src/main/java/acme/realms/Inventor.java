1dda6c4dd88c739ea2e096710a4b1dff82ddb0bb
José Javier Morán Corbacho <josmorcor3@alum.us.es>
2026-02-28 00:01:45 +0100

    Crear sample data para las entidades del requisito S1/1

--- src/main/java/acme/realms/Inventor.java
+++ src/main/java/acme/realms/Inventor.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.validation.Mandatory;
+import acme.constraints.ValidText;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,12 +23,12 @@ public class Inventor extends AbstractRole {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	// @ValidText
+	@ValidText
 	@Column
 	private String				bio;
 
 	@Mandatory
-	// @ValidText
+	@ValidText
 	@Column
 	private String				keyWords;
 
