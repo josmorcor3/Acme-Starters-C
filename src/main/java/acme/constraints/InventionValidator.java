cd8a6c189d8bdc298c2cdf0706ec24bf1900489b
José Javier Morán Corbacho <josmorcor3@alum.us.es>
2026-03-14 21:33:47 +0100

    Modificar validación InventionValidator

--- src/main/java/acme/constraints/InventionValidator.java
+++ src/main/java/acme/constraints/InventionValidator.java
@@ -54,28 +54,19 @@ public class InventionValidator extends AbstractValidator<ValidInvention, Invent
 				super.state(context, publishedInventionHasAtLeastOnePart, "*", "acme.validation.invention.published-without-parts.message");
 			}
 			{
-				boolean startMomentIsNotNull;
-				startMomentIsNotNull = invention.getStartMoment() != null;
-				super.state(context, startMomentIsNotNull, "startMoment", "acme.validation.null-start-moment.message");
-			}
-			{
-				boolean endMomentIsNotNull;
-				endMomentIsNotNull = invention.getEndMoment() != null;
-				super.state(context, endMomentIsNotNull, "endMoment", "acme.validation.null-end-moment.message");
-			}
-			{
 				boolean startMomentIsBeforeEndMoment;
 
-				startMomentIsBeforeEndMoment = MomentHelper.isBefore(invention.getStartMoment(), invention.getEndMoment());
-
-				super.state(context, startMomentIsBeforeEndMoment, "time interval", "acme.validation.invalid-time-interval.message");
+				if(invention.getStartMoment() != null && invention.getEndMoment() != null) {
+					startMomentIsBeforeEndMoment =  MomentHelper.isBefore(invention.getStartMoment(), invention.getEndMoment());
+					super.state(context, startMomentIsBeforeEndMoment, "startMoment", "acme.validation.invalid-time-interval.message");
+				}
 			}
 			{
 				boolean correctCurrency;
 
 				correctCurrency = this.repository.computeInventionCurrencies(invention.getId()).stream().allMatch(c -> c.equals("EUR"));
 
-				super.state(context, correctCurrency, "money currency", "acme.validation.invalid-currency.message");
+				super.state(context, correctCurrency, "cost", "acme.validation.invalid-currency.message");
 			}
 			result = !super.hasErrors(context);
 		}
