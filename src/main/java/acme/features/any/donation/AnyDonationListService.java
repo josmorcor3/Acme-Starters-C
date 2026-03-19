05f9b4ffe7edb2b0934f57b995ee434ff7396fec
luciabaltasar2 <luciabaltasar2@gmail.com>
2026-03-19 19:44:03 +0100

    Cambios pequeños

--- src/main/java/acme/features/any/donation/AnyDonationListService.java
+++ src/main/java/acme/features/any/donation/AnyDonationListService.java
@@ -38,7 +38,7 @@ public class AnyDonationListService extends AbstractService<Any, Donation> {
 	public void authorise() {
 		boolean status;
 
-		status = this.donations != null && this.sponsorship.getDraftMode() == false;
+		status = this.donations != null && !this.sponsorship.getDraftMode();
 
 		super.setAuthorised(status);
 	}
